36f299053634b3c9fb65472289ced163e2714126
mattgibbs741 <mattgibbs741@hotmail.com>
2026-07-03 11:57:14 -0400

    Webpack changes

---
+++ ui/src/IssueAdd.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      status: 'New',
+    };
+
+    const { createIssue } = this.props;
+    createIssue(issue);
+
+    form.owner.value = '';
+    form.title.value = '';
+  }
+
+  render() {
+    return (
+      <form name="issueAdd" onSubmit={this.handleSubmit}>
+        <input type="text" name="owner" placeholder="Owner" />
+        <input type="text" name="title" placeholder="Title" />
+        <button type="submit">Add</button>
+      </form>
+    );
+  }
+}
+
+IssueAdd.propTypes = {
+  createIssue: PropTypes.func.isRequired,
+};
